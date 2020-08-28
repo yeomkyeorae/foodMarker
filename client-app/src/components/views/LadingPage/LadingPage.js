c3768281d3285b3bc0340ab7bf92b02574dfe958
yeomkyeorae <duarufp06@naver.com>
2020-08-28 23:01:52 +0900

    created landing page

---
+++ client-app/src/components/views/LadingPage/LadingPage.js
@@ -0,0 +1,21 @@
+import React, { useEffect } from "react";
+
+function LadingPage() {
+  useEffect(() => {}, []);
+
+  return (
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        width: "100%",
+        height: "100vh"
+      }}
+    >
+      <h2>Landing Page</h2>
+    </div>
+  );
+}
+
+export default LadingPage;
