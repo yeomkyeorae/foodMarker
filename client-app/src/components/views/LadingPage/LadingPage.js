c3e4b67cd7a541bc8a3d19811a2fd0e14a8176a8
yeomkyeorae <duarufp06@naver.com>
2020-08-30 00:39:22 +0900

    add background image

--- client-app/src/components/views/LadingPage/LadingPage.js
+++ client-app/src/components/views/LadingPage/LadingPage.js
@@ -2,7 +2,14 @@ import React, { useEffect } from "react";
 import styled, { css } from "styled-components";
 
 const Section = styled.section`
-  background-color: yellow;
+  width: 100%;
+  height: 100%;
+  margin: auto;
+  background: url("https://images.unsplash.com/photo-1505935428862-770b6f24f629?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1947&q=80")
+    no-repeat 50% 50%;
+  top: 0;
+  display: table;
+  background-size: cover;
 `;
 
 function LadingPage() {
