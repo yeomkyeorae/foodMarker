9cb61b538ebb4fbeb7c4d0c3a80965b8df8ca011
yeomkyeorae <duarufp06@naver.com>
2020-08-30 00:54:12 +0900

    make fullscreen image

--- client-app/src/App.js
+++ client-app/src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import "./App.css";
 
 import LandingPage from "./components/views/LadingPage/LadingPage";
 
