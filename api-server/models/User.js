7f37b64454b9294c30418013535d32726259598e
yeomkyeorae <duarufp06@naver.com>
2020-09-01 22:43:01 +0900

    decode token

--- api-server/models/User.js
+++ api-server/models/User.js
@@ -63,3 +63,23 @@ userSchema.methods.generateToken = function(cb) {
     cb(null, user);
   });
 };
+
+userSchema.statistics.findByToken = function(token, cb) {
+  let user = this;
+
+  // 토큰을 복호화
+  jwt.verify(token, "thisIsSecretToken", function(err, decoded) {
+    // 유저 아이디를 이용해서 유저를 찾은 다음에
+    // 클라이언트에서 가져온 토큰과 데이터베이스에 보관된 토큰이 일치하는지 확인
+    user.findOne({ _id: decoded, token: token }, function(err, user) {
+      if (err) return cb(err);
+      cb(null, user);
+    });
+  });
+};
+
+const User = mongoose.model("User", userSchema);
+
+module.exports = {
+  User
+};
