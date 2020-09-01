614d021138fe35ec9885a0f006c009eac43ff230
yeomkyeorae <duarufp06@naver.com>
2020-09-01 22:51:15 +0900

    add packages at index.js

--- api-server/index.js
+++ api-server/index.js
@@ -2,6 +2,17 @@ const express = require("express");
 const app = express();
 const port = 5000;
 
+const { User } = require("./models/User");
+const { auth } = require("./middleware/auth");
+const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
+const mongoose = require("mongoose");
+const config = require("./config/key");
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(cookieParser());
+
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${[port]}`);
 });
