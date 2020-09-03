46edef9672ff56f4a815842921e653dab66e1cb5
yeomkyeorae <duarufp06@naver.com>
2020-09-03 22:00:00 +0900

    api - auth & logout

--- api-server/index.js
+++ api-server/index.js
@@ -79,6 +79,27 @@ app.post("/api/users/login", (req, res) => {
   });
 });
 
+// auth
+app.get("/api/users/auth", auth, (req, res) => {
+  res.status(200).json({
+    _id: req.user._id,
+    isAuth: true,
+    email: req.user.email,
+    name: req.user.name,
+    role: req.user.role
+  });
+});
+
+// logout
+app.get("/api/users/logout", auth, (req, res) => {
+  User.findOneAndUpdate({ _id: req.user._id }, { token: "" }, (err, user) => {
+    if (err) return res.json({ success: false, err });
+    return res.status(200).send({
+      success: true
+    });
+  });
+});
+
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${[port]}`);
 });
