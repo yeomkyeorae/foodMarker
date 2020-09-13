7473fcb0bffca50b3d5ee3419d1cdb78ff8c9eb7
yeomkyeorae <duarufp06@naver.com>
2020-09-13 13:45:55 +0900

    logout

--- client-app/src/components/views/MainPage/MainPage.js
+++ client-app/src/components/views/MainPage/MainPage.js
@@ -1,8 +1,24 @@
 import React from "react";
+import axios from "axios";
 import { withRouter } from "react-router-dom";
 
-function MainPage() {
-  return <div>hello</div>;
+function MainPage(props) {
+  const onClickHandler = () => {
+    axios.get("/api/users/logout").then(response => {
+      if (response.data.success) {
+        props.history.push("/loginSignup");
+      } else {
+        alert("failed to logout");
+      }
+    });
+  };
+
+  return (
+    <div>
+      hello
+      <button onClick={onClickHandler}>로그아웃</button>
+    </div>
+  );
 }
 
 export default withRouter(MainPage);
