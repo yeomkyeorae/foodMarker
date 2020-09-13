bb335e5193a9444b303e46b2fde8aaa502a2ef8e
yeomkyeorae <duarufp06@naver.com>
2020-09-13 21:56:05 +0900

    add kakao map on MainPage

--- client-app/src/components/views/MainPage/MainPage.js
+++ client-app/src/components/views/MainPage/MainPage.js
@@ -1,8 +1,20 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
 import axios from "axios";
 import { withRouter } from "react-router-dom";
+const { kakao } = window;
 
 function MainPage(props) {
+  const mapContainer = useRef();
+  useEffect(() => {
+    kakao.maps.load(() => {
+      let container = document.getElementById("map");
+      let map = new kakao.maps.Map(container, {
+        center: new kakao.maps.LatLng(33.450701, 126.570667), //지도의 중심좌표.
+        level: 3 //지도의 레벨(확대, 축소 정도)
+      });
+    });
+  }, []);
+
   const onClickHandler = () => {
     axios.get("/api/users/logout").then(response => {
       if (response.data.success) {
@@ -15,8 +27,8 @@ function MainPage(props) {
 
   return (
     <div>
-      hello
       <button onClick={onClickHandler}>로그아웃</button>
+      <div id={`map`} style={{ width: "500px", height: "400px" }}></div>
     </div>
   );
 }
