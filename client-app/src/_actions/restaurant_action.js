9f4ead814ee6adbd051b97a390c93d06d19742c0
yeomkyeorae <duarufp06@naver.com>
2020-09-21 22:36:45 +0900

    add readRestaurants restaurant_action

---
+++ client-app/src/_actions/restaurant_action.js
@@ -0,0 +1,13 @@
+import axios from "axios";
+import { READ_RESTAURANTS } from "./types";
+
+export function loginUser(dataToSubmit) {
+  const request = axios
+    .post("/api/restaurants", dataToSubmit)
+    .then(response => response.data);
+
+  return {
+    type: READ_RESTAURANTS,
+    payload: request
+  };
+}
