0c487286782e595ebeb820361249eb191563ed00
yeomkyeorae <duarufp06@naver.com>
2020-09-22 23:52:28 +0900

    block infinite loop useEffect temporalily

--- client-app/src/components/views/MainPage/RestaurantList.js
+++ client-app/src/components/views/MainPage/RestaurantList.js
@@ -5,19 +5,22 @@ import { readRestaurants } from "../../../_actions/restaurant_action";
 
 function RestaurantList(props) {
   const dispatch = useDispatch();
-  const [restaurants, setRestaurants] = useState([]);
+  const [restaurants, setRestaurants] = useState([{_id: 0}]);
   const body = {
     "id": "5f5a349a5a348908bb89e550"
   }
+  const test = 1;
   useEffect(() => {
     dispatch(readRestaurants(body)).then(response => {
-      console.log(response.payload)
+      setRestaurants(response.payload);
     });
-  });
+  }, [restaurants[0]._id]);
 
   return (
     <div>
-      {restaurants}
+      <ul>
+        {restaurants.map(restaurant => <li key={restaurant._id}>{restaurant.name}</li>)}
+      </ul>
     </div>
   );
 }
