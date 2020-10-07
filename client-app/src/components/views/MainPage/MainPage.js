ee345964fde3106035a975be62cb8cbe24732a4c
yeomkyeorae <duarufp06@naver.com>
2020-10-07 22:49:04 +0900

    move to the map by restaurant name

--- client-app/src/components/views/MainPage/MainPage.js
+++ client-app/src/components/views/MainPage/MainPage.js
@@ -18,6 +18,36 @@ function MainPage(props) {
         center: new kakao.maps.LatLng(33.450701, 126.570667), //지도의 중심좌표.
         level: 3 //지도의 레벨(확대, 축소 정도)
       });
+
+      let geocoder = new kakao.maps.services.Geocoder();
+
+      // 주소로 좌표를 검색합니다
+      // 서울 중구 창경궁로 62-29
+      geocoder.addressSearch("서울 중구 창경궁로 62-29", function(
+        result,
+        status
+      ) {
+        // 정상적으로 검색이 완료됐으면
+        if (status === kakao.maps.services.Status.OK) {
+          let coords = new kakao.maps.LatLng(result[0].y, result[0].x);
+
+          // 결과값으로 받은 위치를 마커로 표시합니다
+          let marker = new kakao.maps.Marker({
+            map: map,
+            position: coords
+          });
+
+          // 인포윈도우로 장소에 대한 설명을 표시합니다
+          let infowindow = new kakao.maps.InfoWindow({
+            content:
+              '<div style="width:150px;text-align:center;padding:6px 0;">우래옥</div>'
+          });
+          infowindow.open(map, marker);
+
+          // 지도의 중심을 결과값으로 받은 위치로 이동시킵니다
+          map.setCenter(coords);
+        }
+      });
     });
   }, []);
 
