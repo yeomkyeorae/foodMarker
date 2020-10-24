58788633a5ca493187ba79a4b04d96633b1dbc02
yeomkyeorae <duarufp06@naver.com>
2020-10-24 14:55:17 +0900

    style:

--- client-app/src/components/views/Navbar/NavbarComp.js
+++ client-app/src/components/views/Navbar/NavbarComp.js
@@ -1,25 +1,29 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import { Navbar, Nav } from "react-bootstrap";
+import styled from "styled-components";
+
+const NavDiv = styled.div`
+  color: white;
+  font-size: 1rem;
+  margin: 10px;
+`;
 
 function NavbarComp(props) {
-  console.log("Nav props: ", props);
   const userId = props.userId;
-  // const onClickHandler = () => {
-  //   props.history.push({
-  //     pathname: "/wish",
-  //     state: props.userId
-  //   });
-  // };
 
   return (
     <Navbar bg="primary" variant="dark">
-      <Navbar.Brand href="/">Food Marker</Navbar.Brand>
+      <Navbar.Brand href="/" style={{ color: "skyblue" }}>
+        Food Marker
+      </Navbar.Brand>
       <Nav className="mr-auto">
-        {/* <Nav.Link href="/main">My Restaurants</Nav.Link>
-        <Nav.Link href="/wish">Wish List</Nav.Link> */}
-        <Link to={{ pathname: "/main", state: userId }}>main</Link>
-        <Link to={{ pathname: "/wish", state: userId }}>wish</Link>
+        <Link to={{ pathname: "/main", state: userId }}>
+          <NavDiv>My Restaurants</NavDiv>
+        </Link>
+        <Link to={{ pathname: "/wish", state: userId }}>
+          <NavDiv>Wish Retaurants</NavDiv>
+        </Link>
       </Nav>
     </Navbar>
   );
