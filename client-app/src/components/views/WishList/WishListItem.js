65cf7c2729d6ab26c30c03abc918b1c7bb1e1d3f
yeomkyeorae <duarufp06@naver.com>
2020-11-10 22:43:19 +0900

    fix: date to only date

--- client-app/src/components/views/WishList/WishListItem.js
+++ client-app/src/components/views/WishList/WishListItem.js
@@ -32,7 +32,7 @@ function WishListItem(props) {
   };
 
   const onVisitiedDateHandler = e => {
-    setVisitiedDate(e.currentTarget.value);
+    setVisitiedDate(String(e.currentTarget.value));
   };
 
   const moveToMain = () => {
