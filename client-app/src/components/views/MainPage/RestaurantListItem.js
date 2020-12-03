d52bcc16afff2c47e382bbf97101dd998a8c40bc
yeomkyeorae <duarufp06@naver.com>
2020-12-03 23:29:50 +0900

    rm: unused tag

--- client-app/src/components/views/MainPage/RestaurantListItem.js
+++ client-app/src/components/views/MainPage/RestaurantListItem.js
@@ -7,27 +7,6 @@ import heic2any from "heic2any";
 import axios from "axios";
 import styled from "styled-components";
 
-const Item = styled.li`
-  display: block;
-  clear: both;
-  counter-increment: list;
-  padding-bottom: 4rem;
-  font-size: 1.1rem;
-  line-height: 1.375;
-  position: relative;
-`;
-
-const HeadLine = styled.h2`
-  padding: 0rem 0 0 0;
-  margin: 0 0 1rem 0;
-  font: normal 2rem var(--font-head);
-`;
-
-const FoodImg = styled.img`
-  max-width: 100%;
-  height: auto;
-`;
-
 function RestaurantListItem(props) {
   const restaurant = props.restaurant;
   const [Toggle, setToggle] = useState(false);
