e59c9b01d72e6b43a5dc84c1de3313dfb4157ae2
yeomkyeorae <duarufp06@naver.com>
2021-01-10 22:44:42 +0900

    fix: immediately-invoked function to normal about infoWindow

--- client-app/src/components/views/MainPage/Enroll.js
+++ client-app/src/components/views/MainPage/Enroll.js
@@ -79,6 +79,29 @@ function Enroll(props) {
         }
       }
 
+      function infoWindow(marker, title, itemEl, addrress_name) {
+        kakao.maps.event.addListener(marker, "mouseover", function() {
+          displayInfowindow(marker, title);
+        });
+
+        kakao.maps.event.addListener(marker, "mouseout", function() {
+          infowindow.close();
+        });
+
+        itemEl.onmouseover = function() {
+          displayInfowindow(marker, title);
+        };
+
+        itemEl.onclick = () => {
+          setName(title);
+          setAddress(addrress_name);
+        };
+
+        itemEl.onmouseout = function() {
+          infowindow.close();
+        };
+      }
+
       // 검색 결과 목록과 마커를 표출하는 함수입니다
       function displayPlaces(places) {
         var listEl = document.getElementById("placesList"),
@@ -105,28 +128,7 @@ function Enroll(props) {
           // 마커와 검색결과 항목에 mouseover 했을때
           // 해당 장소에 인포윈도우에 장소명을 표시합니다
           // mouseout 했을 때는 인포윈도우를 닫습니다
-          (function(marker, title) {
-            kakao.maps.event.addListener(marker, "mouseover", function() {
-              displayInfowindow(marker, title);
-            });
-
-            kakao.maps.event.addListener(marker, "mouseout", function() {
-              infowindow.close();
-            });
-
-            itemEl.onmouseover = function() {
-              displayInfowindow(marker, title);
-            };
-
-            itemEl.onclick = () => {
-              setName(title);
-              setAddress(addrress_name);
-            };
-
-            itemEl.onmouseout = function() {
-              infowindow.close();
-            };
-          })(marker, places[i].place_name);
+          infoWindow(marker, places[i].place_name, itemEl, addrress_name);
 
           fragment.appendChild(itemEl);
         }
