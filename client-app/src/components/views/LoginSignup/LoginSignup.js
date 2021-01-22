6b93562999759d9d22d2b511283a502405416b1b
yeomkyeorae <duarufp06@naver.com>
2021-01-22 23:50:33 +0900

    style: save background img in assets about loginSignup page

--- client-app/src/components/views/LoginSignup/LoginSignup.js
+++ client-app/src/components/views/LoginSignup/LoginSignup.js
@@ -2,13 +2,13 @@ import React, { useEffect, useState } from "react";
 import styled from "styled-components";
 import Login from "./Login";
 import Signup from "./Signup";
+import background from "../../../assets/loginBackground.jpeg";
 
 const Section = styled.section`
   width: 100%;
   height: 100%;
   margin: auto;
-  background: url("https://images.unsplash.com/photo-1477617722074-45613a51bf6d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2100&q=80")
-    no-repeat 50% 50%;
+  background: url(${background}) no-repeat 50% 50%;
   top: 0;
   display: table;
   background-size: cover;
