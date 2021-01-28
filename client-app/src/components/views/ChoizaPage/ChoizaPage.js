16db2d0d46f5376aea759dfe80bfc29e3b726669
yeomkyeorae <duarufp06@naver.com>
2021-01-28 23:17:05 +0900

    feat: get choizaroad videos by youtube api

--- client-app/src/components/views/ChoizaPage/ChoizaPage.js
+++ client-app/src/components/views/ChoizaPage/ChoizaPage.js
@@ -1,9 +1,20 @@
-import React from "react";
+import React, { useEffect } from "react";
+import axios from "axios";
 import { withRouter } from "react-router-dom";
 import NavbarComp from "../Navbar/NavbarComp";
 
 function ChoizaPage(props) {
   const userId = props.location.state;
+
+  useEffect(() => {
+    axios
+      .get(
+        "https://www.googleapis.com/youtube/v3/search?key=AIzaSyAr_Xi495739tXcKNd_obqC0_v0dj6-m7A&channelId=UCgiO7Kxib0SZEG0DoeuBkdQ&part=snippet"
+      )
+      .then(response => {
+        console.log(response);
+      });
+  });
 
   return (
     <div style={{ width: "100%", height: "100%", textAlign: "center" }}>
