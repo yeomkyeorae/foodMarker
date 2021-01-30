74b551a43d436044fc06cc3688d1ed5cf749b614
yeomkyeorae <duarufp06@naver.com>
2021-01-30 23:15:14 +0900

    feat: split Restaurants and Kakao map component

--- client-app/src/components/views/ChoizaPage/ChoizaPage.js
+++ client-app/src/components/views/ChoizaPage/ChoizaPage.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import axios from "axios";
 import { withRouter } from "react-router-dom";
 import NavbarComp from "../Navbar/NavbarComp";
@@ -6,21 +6,27 @@ import { youtubeURI } from "../../../config/dev";
 
 function ChoizaPage(props) {
   const userId = props.location.state;
+  const [choizaItem, setChoizaItem] = useState("");
 
   useEffect(() => {
-    axios
-      .get(
-        `https://www.googleapis.com/youtube/v3/search?key=${youtubeURI}&channelId=UCgiO7Kxib0SZEG0DoeuBkdQ&part=snippet`
-      )
-      .then(response => {
-        console.log(response);
-      });
+    // axios
+    //   .get(
+    //     `https://www.googleapis.com/youtube/v3/search?key=${youtubeURI}&channelId=UCgiO7Kxib0SZEG0DoeuBkdQ&part=snippet&maxResults=20`
+    //   )
+    //   .then(response => {
+    //     console.log(response);
+    //     setChoizaItem(response.data.items);
+    //     console.log(choizaItem);
+    //   });
   });
 
   return (
     <div style={{ width: "100%", height: "100%", textAlign: "center" }}>
       <NavbarComp userId={userId} history={props.history} />
       This is ChoizaPage
+      {/* {choizaItem.map(item => (
+        <h1>item.snippet.title</h1>
+      ))} */}
     </div>
   );
 }
--- client-app/src/components/views/MarkerPage/Restaurants.js
+++ client-app/src/components/views/MarkerPage/Restaurants.js
@@ -1,63 +1,16 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import { withRouter } from "react-router-dom";
 import RestaurantList from "./RestaurantList";
-
-const { kakao } = window;
+import KakaoMap from "../../containers/KakaoMap/KakaoMap";
 
 function Restaurants(props) {
   const userId = props.userId;
   const [address, setAddress] = useState("서울 중구 창경궁로 62-29");
   const [restaurantName, setRestaurantName] = useState("우래옥");
 
-  useEffect(() => {
-    kakao.maps.load(() => {
-      let container = document.getElementById("map");
-      let map = new kakao.maps.Map(container, {
-        center: new kakao.maps.LatLng(33.450701, 126.570667), //지도의 중심좌표.
-        level: 3 //지도의 레벨(확대, 축소 정도)
-      });
-
-      let geocoder = new kakao.maps.services.Geocoder();
-
-      // 주소로 좌표를 검색합니다
-      // 서울 중구 창경궁로 62-29
-      geocoder.addressSearch(address, function(result, status) {
-        // 정상적으로 검색이 완료됐으면
-        if (status === kakao.maps.services.Status.OK) {
-          let coords = new kakao.maps.LatLng(result[0].y, result[0].x);
-
-          // 결과값으로 받은 위치를 마커로 표시합니다
-          let marker = new kakao.maps.Marker({
-            map: map,
-            position: coords
-          });
-
-          // 인포윈도우로 장소에 대한 설명을 표시합니다
-          let infowindow = new kakao.maps.InfoWindow({
-            content:
-              '<div style="width:150px;text-align:center;padding:6px 0;">' +
-              restaurantName +
-              "</div>"
-          });
-          infowindow.open(map, marker);
-
-          // 지도의 중심을 결과값으로 받은 위치로 이동시킵니다
-          map.setCenter(coords);
-        }
-      });
-    });
-  });
-
   return (
     <div style={{ display: "inline-block", width: "100%" }}>
-      <div
-        id={`map`}
-        style={{
-          width: "45%",
-          height: "500px",
-          display: "inline-block"
-        }}
-      ></div>
+      <KakaoMap address={address} restaurantName={restaurantName} />
       <RestaurantList
         userId={userId}
         setAddress={setAddress}
