2cef532ce591357b3de9db2e81f77a59495adc1d
yeomkyeorae <duarufp06@naver.com>
2021-02-02 22:09:36 +0900

    feat: render KakaoMap on MainPage

--- client-app/src/components/views/MainPage/MainPage.js
+++ client-app/src/components/views/MainPage/MainPage.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { withRouter } from "react-router-dom";
 import NavbarComp from "../Navbar/NavbarComp";
+import KakaoMap from "../../containers/KakaoMap/KakaoMap";
 
 function MainPage(props) {
   const userId = props.location.state;
@@ -8,7 +9,7 @@ function MainPage(props) {
   return (
     <div style={{ width: "100%", height: "100%", textAlign: "center" }}>
       <NavbarComp userId={userId} history={props.history} />
-      This is MainPage
+      <KakaoMap />
     </div>
   );
 }
