32cb97937ec869df6d24a98035b46167cdf204e3
yeomkyeorae <duarufp06@naver.com>
2021-02-17 22:30:27 +0900

    style: RestaurantListItem update popup fixed more fancy

--- client-app/src/components/views/MarkerPage/RestaurantListItem.js
+++ client-app/src/components/views/MarkerPage/RestaurantListItem.js
@@ -83,39 +83,40 @@ function RestaurantListItem(props) {
       });
   };
 
-  const ModalComp = (
-    <Modal.Dialog
-      style={{
-        textAlign: "center",
-        position: "fixed",
-        zIndex: "100",
-        left: "35%",
-        top: "30%"
-      }}
-    >
-      <Modal.Header closeButton onClick={() => openPopUp()}>
-        <Modal.Title>{restaurant.name}</Modal.Title>
-      </Modal.Header>
-      <Modal.Body>
-        <input
-          type="date"
-          value={VisitiedDate}
-          placeholder="방문 일시"
-          onChange={onVisitiedDateHandler}
-        />
-      </Modal.Body>
-      <Modal.Footer>
-        <div style={{ marginLeft: "100px" }}>
-          <input type="file" onChange={onImageDataHandler} />
-        </div>
-      </Modal.Footer>
-      <Modal.Footer>
-        <Button variant="success" onClick={changeRestaurant}>
-          수정하기
-        </Button>
-      </Modal.Footer>
-    </Modal.Dialog>
-  );
+  const ModalComp = props => {
+    return (
+      <Modal
+        {...props}
+        style={{
+          textAlign: "center"
+        }}
+        aria-labelledby="contained-modal-title-vcenter"
+        centered
+      >
+        <Modal.Header closeButton onClick={() => openPopUp()}>
+          <Modal.Title>{restaurant.name}</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <input
+            type="date"
+            value={VisitiedDate}
+            placeholder="방문 일시"
+            onChange={onVisitiedDateHandler}
+          />
+        </Modal.Body>
+        <Modal.Footer>
+          <div style={{ marginLeft: "100px" }}>
+            <input type="file" onChange={onImageDataHandler} />
+          </div>
+        </Modal.Footer>
+        <Modal.Footer>
+          <Button variant="success" onClick={changeRestaurant}>
+            수정하기
+          </Button>
+        </Modal.Footer>
+      </Modal>
+    );
+  };
 
   const updateHandler = e => {
     setToggle(!Toggle);
@@ -128,64 +129,70 @@ function RestaurantListItem(props) {
   };
 
   return (
-    <Col md={6}>
-      {Toggle && ModalComp}
-      <Card
-        style={{ width: "100%" }}
-        onClick={() => clickRestaurant(restaurant.address, restaurant.name)}
-      >
-        <Card.Body>
-          <Card.Title>{restaurant.name}</Card.Title>
-          <ReactStars
-            count={5}
-            value={3.5}
-            edit={false}
-            size={52}
-            isHalf={true}
-            activeColor="#ffd700"
-          />
-          ,
-          <div
-            style={{
-              width: "100%",
-              height: "360px",
-              overflow: "hidden"
-            }}
-          >
-            <Card.Img
-              variant="top"
-              src={restaurant.imgURL}
+    <>
+      <Col md={6}>
+        <Card
+          style={{ width: "100%" }}
+          onClick={() => clickRestaurant(restaurant.address, restaurant.name)}
+        >
+          <Card.Body>
+            <Card.Title>{restaurant.name}</Card.Title>
+            <ReactStars
+              count={5}
+              value={3.5}
+              edit={false}
+              size={52}
+              isHalf={true}
+              activeColor="#ffd700"
+            />
+            ,
+            <div
               style={{
                 width: "100%",
-                height: "100%"
+                height: "360px",
+                overflow: "hidden"
               }}
-            />
-          </div>
-          <div>
-            <Card.Text style={{ display: "inline-block" }}>
-              방문 일시: {restaurant.date} <br />
-              주소: {restaurant.address}
-            </Card.Text>
-          </div>
-          <div>
-            <Button
-              variant="warning"
-              onClick={() => updateHandler()}
-              style={{ display: "inline-block", margin: "2px", color: "White" }}
             >
-              수정
-            </Button>
-            <Button
-              variant="primary"
-              onClick={() => props.deleteHandler(restaurant._id)}
-              style={{ display: "inline-block", margin: "2px" }}
-            >
-              삭제
-            </Button>
-          </div>
-        </Card.Body>
-      </Card>
-    </Col>
+              <Card.Img
+                variant="top"
+                src={restaurant.imgURL}
+                style={{
+                  width: "100%",
+                  height: "100%"
+                }}
+              />
+            </div>
+            <div>
+              <Card.Text style={{ display: "inline-block" }}>
+                방문 일시: {restaurant.date} <br />
+                주소: {restaurant.address}
+              </Card.Text>
+            </div>
+            <div>
+              <Button
+                variant="warning"
+                onClick={() => updateHandler()}
+                style={{
+                  display: "inline-block",
+                  margin: "2px",
+                  color: "White"
+                }}
+              >
+                수정
+              </Button>
+              <Button
+                variant="primary"
+                onClick={() => props.deleteHandler(restaurant._id)}
+                style={{ display: "inline-block", margin: "2px" }}
+              >
+                삭제
+              </Button>
+            </div>
+          </Card.Body>
+        </Card>
+      </Col>
+      <ModalComp show={Toggle} onHide={() => setToggle(false)} />
+    </>
   );
 }
 
