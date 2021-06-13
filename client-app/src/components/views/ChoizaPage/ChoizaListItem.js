d76746516d76798f060c5c2513ccf390a0b54f61
yeomkyeorae <duarufp06@naver.com>
2021-06-13 22:15:35 +0900

    feat: add tooltip when hover choizaRoadItem

--- client-app/src/components/views/ChoizaPage/ChoizaListItem.js
+++ client-app/src/components/views/ChoizaPage/ChoizaListItem.js
@@ -1,43 +1,53 @@
-import React from "react";
+import React, { useState } from "react";
+import styled from "styled-components";
 import { withRouter } from "react-router-dom";
-import { Card, Col } from "react-bootstrap";
+import { Card, Col, OverlayTrigger, Tooltip } from "react-bootstrap";
 
 function ChoizaListItem(props) {
   const choizaRoad = props.choizaRoad;
+  const [showTooltip, setShowTooltip] = useState(false);
 
   const clickChoizaRoad = URL => {
     window.open(URL, "_blank");
   };
 
   return (
-    <Col md={3}>
-      <Card
-        style={{ width: "100%", height: "100%", cursor: "pointer" }}
-        onClick={() => clickChoizaRoad(choizaRoad.youtubeURL)}
-      >
-        <Card.Body>
-          <div
-            style={{
-              width: "100%",
-              height: "240px",
-              overflow: "hidden"
-            }}
-          >
-            <Card.Img
-              variant="top"
-              src={choizaRoad.thumbnailURL}
+    <OverlayTrigger
+      key="bottom"
+      placement="bottom"
+      overlay={<Tooltip id={`tooltip-bottom`}>툴팁 테스트</Tooltip>}
+    >
+      <Col md={3}>
+        <Card
+          style={{ width: "100%", height: "100%", cursor: "pointer" }}
+          onClick={() => clickChoizaRoad(choizaRoad.youtubeURL)}
+          onMouseEnter={() => setShowTooltip(true)}
+          onMouseLeave={() => setShowTooltip(false)}
+        >
+          <Card.Body>
+            <div
               style={{
                 width: "100%",
-                height: "100%"
+                height: "240px",
+                overflow: "hidden"
               }}
-            />
-          </div>
-          <p style={{ fontFamily: "Do Hyeon", fontSize: "20px" }}>
-            {choizaRoad.title}
-          </p>
-        </Card.Body>
-      </Card>
-    </Col>
+            >
+              <Card.Img
+                variant="top"
+                src={choizaRoad.thumbnailURL}
+                style={{
+                  width: "100%",
+                  height: "100%"
+                }}
+              />
+            </div>
+            <p style={{ fontFamily: "Do Hyeon", fontSize: "20px" }}>
+              {choizaRoad.title}
+            </p>
+          </Card.Body>
+        </Card>
+      </Col>
+    </OverlayTrigger>
   );
 }
 
