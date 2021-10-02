739b852126f7f99eb0a50564c2184d995ae09d3c
yeomkyeorae <duarufp06@naver.com>
2021-10-02 11:06:32 +0900

    style: fixed footer on bottom

--- client-app/src/components/views/Footer/Footer.js
+++ client-app/src/components/views/Footer/Footer.js
@@ -4,11 +4,13 @@ import styled from "styled-components";
 const FooterDiv = styled.footer`
   width: 100%;
   height: 50px;
+  left: 0;
   bottom: 0;
   position: ${props => props.position === "fixed" ? "fixed;" : "relative;"};
   background: gray;
   color: white;
   overflow: hidden;
+  position: fixed;
 `;
 
 const FooterP = styled.p`
