e88c1a8ff27f20cdadf8e8aff02efde3c0c6cbe6
yeomkyeorae <duarufp06@naver.com>
2021-10-19 22:41:03 +0900

    fix: change app path

--- api-server/index.js
+++ api-server/index.js
@@ -473,10 +473,10 @@ app.delete("/api/visitedChoizaRoads", (req, res) => {
   });
 });
 
-app.use(express.static(path.join(__dirname, 'client-app/dist')));
+app.use(express.static(path.join(__dirname, '../client-app/dist')));
 
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname + 'client-app/dist/index.html'));
+  res.sendFile(path.join(__dirname, 'client-app/dist/index.html'));
 });
 
 app.listen(port, () => {
