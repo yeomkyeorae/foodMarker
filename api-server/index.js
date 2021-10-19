aa802b690ae18f34df1670913a336cc578110c91
yeomkyeorae <duarufp06@naver.com>
2021-10-19 22:05:16 +0900

    build: heroku

--- api-server/index.js
+++ api-server/index.js
@@ -473,6 +473,12 @@ app.delete("/api/visitedChoizaRoads", (req, res) => {
   });
 });
 
+app.use(express.static(path.join(__dirname, 'client-app/dist')));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname + 'client-app/dist/index.html'));
+});
+
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${[port]}`);
 });
