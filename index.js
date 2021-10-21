0b74fe526146a51da2caf8e4abb65daec49532fe
yeomkyeorae <duarufp06@naver.com>
2021-10-21 22:24:34 +0900

    deploy: change scripts start to concurrently

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-const port = 5000;
+const port = process.env.PORT || 5000;
 
 const { User } = require("./models/User");
 const { Restaurant } = require("./models/Restaurant");
