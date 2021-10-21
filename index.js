d650d2b3e31154196b5fe3d009f8995cbc31ab8e
yeomkyeorae <duarufp06@naver.com>
2021-10-21 23:42:29 +0900

    deploy: change scripts

--- index.js
+++ index.js
@@ -475,11 +475,14 @@ app.delete("/api/visitedChoizaRoads", (req, res) => {
   });
 });
 
-app.use(express.static(path.join(__dirname, 'client-app/dist')));
+if(process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, 'client-app/dist')));
+  
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'client-app/dist/index.html'));
+  });
+}
 
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'client-app/dist/index.html'));
-});
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${[port]}`);
