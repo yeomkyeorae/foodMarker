1e909c265c29984955d4eaeaf858393306d87905
yeomkyeorae <duarufp06@naver.com>
2021-10-21 22:31:43 +0900

    deploy: change port as 5000

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-const port = process.env.PORT || 5000;
+const port = 5000;
 
 const { User } = require("./models/User");
 const { Restaurant } = require("./models/Restaurant");
