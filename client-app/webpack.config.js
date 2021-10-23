2c3da7503514bb14087223b6109404a1b0299007
yeomkyeorae <duarufp06@naver.com>
2021-10-23 12:38:02 +0900

    deploy: set webpack to display background

--- client-app/webpack.config.js
+++ client-app/webpack.config.js
@@ -37,8 +37,9 @@ module.exports = {
         test: /\.(jpeg|png|ico)$/,
         loader: "file-loader",
         options: {
-          publicPath: "./dist/", // prefix를 아웃풋 경로로 지정
-          name: "[name].[ext]?[hash]" // 파일명 형식
+          publicPath: "./", // prefix를 아웃풋 경로로 지정
+          // name: "[name].[ext]?[hash]" // 파일명 형식
+          name: "[name].[ext]"
         }
       }
     ]
