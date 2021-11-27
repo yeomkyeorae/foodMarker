adf6d350d1916bc2bd3fa417fc56fc0c4e10ff37
yeomkyeorae <duarufp06@naver.com>
2021-11-27 23:49:26 +0900

    feat: add alert modal

---
+++ client-app/src/components/containers/AlertModal/AlertModal.js
@@ -0,0 +1,33 @@
+import React from "react";
+import { Modal } from "react-bootstrap";
+import { withRouter } from "react-router-dom";
+
+function AlertModal(props) {
+  const { setAlertToggle, alertMessage } = props;
+
+  return (
+    <Modal
+      show={true}
+      onHide={() => setAlertToggle(false)}
+      style={{
+        textAlign: "center"
+      }}
+      aria-labelledby="contained-modal-title-vcenter"
+      centered
+    >
+      <Modal.Header closeButton>알림</Modal.Header>
+      <Modal.Body closeButton>
+        <div
+          style={{
+            margin: "auto",
+            width: "100%"
+          }}
+        >
+            {alertMessage}
+        </div>
+      </Modal.Body>
+    </Modal>
+  );
+}
+
+export default withRouter(AlertModal);
