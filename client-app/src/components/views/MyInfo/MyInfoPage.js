936e8370a0665365019740715b8becad2b529ed6
yeomkyeorae <duarufp06@naver.com>
2022-01-13 23:20:29 +0900

    feat: temporary layer of myInfo

--- client-app/src/components/views/MyInfo/MyInfoPage.js
+++ client-app/src/components/views/MyInfo/MyInfoPage.js
@@ -4,15 +4,15 @@ import NavbarComp from "../Navbar/NavbarComp";
 import Footer from "../Footer/Footer";
 import styled from "styled-components";
 
-const Div = styled.div`
-  display: inline-block;
-  width: 10rem;
-  &:hover {
-    text-shadow: -1px 0 white, 0 1px white, 1px 0 white, 0 -1px white;
-    color: #ff6700;
-  }
-  cursor: pointer;
-`;
+// const Div = styled.div`
+//   display: inline-block;
+//   width: 100%;
+//   &:hover {
+//     text-shadow: -1px 0 white, 0 1px white, 1px 0 white, 0 -1px white;
+//     color: #ff6700;
+//   }
+//   cursor: pointer;
+// `;
 
 function MyInfoPage(props) {
   const userId = props.location.state;
@@ -30,7 +30,41 @@ function MyInfoPage(props) {
       >
         <NavbarComp userId={userId} history={props.history} />
         <hr />
-        <Div>내 정보 페이지</Div>
+        <div>
+            <div style={{ height: "150px"}}>
+                <h1>내 정보 페이지</h1>
+            </div>
+            <div style={{display: "flex", justifyContent: "center"}}>
+                <div style={{ width: "40%"}}>
+                    <h3>등록한 나의 방문 맛집 개수</h3>
+                    <div style={{height: "100px"}}>
+                        12개
+                    </div>
+                </div>
+                <div style={{ width: "40%"}}>
+                    <h3>등록한 나의 위시 맛집 개수</h3>
+                    <div style={{height: "100px"}}>
+                        17개
+                    </div>
+                </div>
+            </div>
+            <div style={{display: "flex", justifyContent: "center"}}>
+                <div style={{ width: "30%"}}>
+                    <h3>지역별 나의 방문/위시 맛집 리스트</h3>
+                    <div style={{height: "100px"}}>
+                        <ul>
+                            <li>서울 10개</li>
+                            <li>대전 10개</li>
+                            <li>충청 10개</li>
+                            <li>세종 10개</li>
+                        </ul>
+                    </div>
+                </div>
+                <div style={{ width: "50%"}}>
+                    <h3>나의 방문 맛집 달력</h3>
+                </div>
+            </div>
+        </div>
         <hr />
         <Footer />
       </div>
