f655f3a1ecd8a80dcce98d3733cff250bc83bd2a
yeomkyeorae <duarufp06@naver.com>
2022-04-07 23:42:54 +0900

    feat: add conditions to call api on useFetch

--- client-app/src/components/views/MarkerPage/useFetch.tsx
+++ client-app/src/components/views/MarkerPage/useFetch.tsx
@@ -7,36 +7,48 @@ import {
 
 const ITEMPERPAGE = 4;
 
-function useFetch(page, order) {
+function useFetch(page, order, totalItemCount) {
     const dispatch = useDispatch<any>();
     const [loading, setLoading] = useState(true);
     const [error, setError] = useState(false);
     const [restaurantList, setRestaurantList] = useState([]);
 
-    const sendQuery = useCallback(async () => {
+    const sendQuery = useCallback(async (totalItemCount) => {
         try {
-            setLoading(true);
-            setError(false);
+            const maxPage = totalItemCount % ITEMPERPAGE === 0 ? Math.floor(totalItemCount / ITEMPERPAGE) : Math.floor(totalItemCount / ITEMPERPAGE) + 1;
 
-            const body = {
-                id: window.sessionStorage.getItem("userId"),
-                page: page,
-                itemPerPage: ITEMPERPAGE,
-                order: order
-            };
-            
-            const newRestaurantList = (await dispatch(readRestaurants(body))).payload;
-            console.log('newRestaurantList', newRestaurantList);
-            
-            setRestaurantList(newRestaurantList);
+            if(maxPage !== 0 && maxPage < page) {
+                setLoading(false);
+                setError(true);
+            } else {
+                setLoading(true);
+                setError(false);
+
+                const body = {
+                    id: window.sessionStorage.getItem("userId"),
+                    page: page,
+                    itemPerPage: ITEMPERPAGE,
+                    order: order
+                };
+                
+                const newRestaurantList = (await dispatch(readRestaurants(body))).payload;
+
+                if(!newRestaurantList.length) {
+                    setLoading(false);
+                    setError(true);
+                } else {
+                    setRestaurantList(restaurantList => restaurantList.concat(newRestaurantList));
+                }
+                
+            }
         } catch(err) {
             setError(false);
         }
     }, [page, order]);
 
     useEffect(() => {
-        sendQuery();
-    }, [sendQuery, page]);
+        sendQuery(totalItemCount);
+    }, [sendQuery, page, totalItemCount]);
 
     return { loading, error, restaurantList }
 }
