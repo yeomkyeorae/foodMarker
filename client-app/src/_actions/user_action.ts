cae1946e258e4938c51a727559e9be5e0e4d94c2
yeomkyeorae <duarufp06@naver.com>
2022-04-24 21:42:09 +0900

    typescript: set type of parameters and return on user_action

--- client-app/src/_actions/user_action.ts
+++ client-app/src/_actions/user_action.ts
@@ -1,10 +1,10 @@
 import axios from "axios";
 import { LOGIN_USER, LOGOUT_USER, REGISTER_USER, AUTH_USER } from "./types";
 
-export function loginUser(dataToSubmit) {
+export function loginUser(dataToSubmit: { email: string, password: string}): { type: string, payload: Promise<{ loginSuccess: boolean, name: string, userId: string}> } {
   const request = axios
     .post("/api/users/login", dataToSubmit)
-    .then(response => response.data);
+    .then(response => response.data );
 
   return {
     type: LOGIN_USER,
@@ -12,7 +12,7 @@ export function loginUser(dataToSubmit) {
   };
 }
 
-export function logoutUser() {
+export function logoutUser(): { type: string, payload: Promise<{ success: boolean }> }{
   const request = axios
     .get("/api/users/logout")
     .then(response => response.data);
@@ -23,7 +23,7 @@ export function logoutUser() {
   };
 }
 
-export function registerUser(dataToSubmit) {
+export function registerUser(dataToSubmit: { email: string, name: string, password: string }): { type: string, payload: Promise<{ success: boolean }>} {
   const request = axios
     .post("/api/users/signup", dataToSubmit)
     .then(response => response.data);
@@ -34,8 +34,9 @@ export function registerUser(dataToSubmit) {
   };
 }
 
-export function auth() {
+export function auth(): { type: string, payload: Promise<{ isAuth: boolean, error: boolean }> } {
   const request = axios.get("/api/users/auth").then(response => response.data);
+
   return {
     type: AUTH_USER,
     payload: request
