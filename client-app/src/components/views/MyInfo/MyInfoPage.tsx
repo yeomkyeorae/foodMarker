abc919f66360d1f876d6de7fc276461a67d0d47e
yeomkyeorae <duarufp06@naver.com>
2022-05-03 23:01:36 +0900

    typescript: set types of parameters and return on MyInfoPage

--- client-app/src/components/views/MyInfo/MyInfoPage.tsx
+++ client-app/src/components/views/MyInfo/MyInfoPage.tsx
@@ -1,18 +1,18 @@
 import React, { useState, useEffect } from "react";
-import { withRouter } from "react-router-dom";
+import { withRouter, RouteComponentProps } from "react-router-dom";
 import { useDispatch } from "react-redux";
-import { readRestaurants ,readRestaurantsCount } from '../../../_actions/restaurant_action';
+import { readRestaurants, readRestaurantsCount } from '../../../_actions/restaurant_action';
 import { readWishList, readWishListCount } from '../../../_actions/wishList_action';
 import NavbarComp from "../Navbar/NavbarComp";
 import Footer from "../Footer/Footer";
 import MyCalendar from "./MyCalendar";
 import styled from "styled-components";
 import { RestaurantDetail } from "../../interfaces/Restaurant";
-import { WishListType } from "../../interfaces/WishList"; 
+import { WishListType } from "../../interfaces/WishList";
 import { NavMenuType } from "../../../library/def";
 
 
-const InfoBlock = styled.div<{backgroundColor?: string;}>`
+const InfoBlock = styled.div<{ backgroundColor?: string; }>`
   width: 25%;
   border-radius: 30px;
   background-color: ${props => props.backgroundColor};
@@ -28,7 +28,7 @@ const InfoContent = styled.span`
   font-size: 1.5rem;
 `;
 
-const ColorBar = styled.div<{backgroundColor?: string;}>`
+const ColorBar = styled.div<{ backgroundColor?: string; }>`
   margin-right: 2px;
   background-color: ${props => props.backgroundColor};
   color: white;
@@ -36,20 +36,25 @@ const ColorBar = styled.div<{backgroundColor?: string;}>`
 `;
 
 const colorBar = [
-  {color: "gold", text: "평점 5점"}, 
-  {color: "#8bc34a", text: "평점 4점대"}, 
-  {color: "#35baf6", text: "평점 3점대"},
-  {color: "#ed4b82", text: "평점 2점대"},
-  {color: "gray", text: "평점 1점대"}
+  { color: "gold", text: "평점 5점" },
+  { color: "#8bc34a", text: "평점 4점대" },
+  { color: "#35baf6", text: "평점 3점대" },
+  { color: "#ed4b82", text: "평점 2점대" },
+  { color: "gray", text: "평점 1점대" }
 ];
 
-function MyInfoPage(props) {
+interface Props {
+  history: RouteComponentProps["history"]
+}
+
+
+function MyInfoPage({ history }: Props): React.ReactElement {
   const [myRestaurantsCount, setMyRestaurantsCount] = useState(0);
   const [myWishListCount, setMyWishListCount] = useState(0);
   const [myRestaurants, setMyRestaurants] = useState<RestaurantDetail[]>([]);
   const [myWishlists, setWishlists] = useState<WishListType[]>([]);
 
-  const { userId } = props.location.state;
+  const userId = window.sessionStorage.getItem("userId") as string;
 
   const dispatch = useDispatch<any>();
 
@@ -102,57 +107,57 @@ function MyInfoPage(props) {
           overflow: "hidden"
         }}
       >
-        <NavbarComp history={props.history} menu={NavMenuType.Myinfo} />
+        <NavbarComp history={history} menu={NavMenuType.Myinfo} />
         <hr />
         <div>
-            <div style={{ height: "150px"}}>
-                <h1>내 정보 페이지</h1>
+          <div style={{ height: "150px" }}>
+            <h1>내 정보 페이지</h1>
+          </div>
+          <div style={{ display: "flex", justifyContent: "center" }}>
+            <InfoBlock backgroundColor={"#5B6DCD"}>
+              <InfoTitle>등록한 나의 방문 맛집 개수</InfoTitle>
+              <div style={{ height: "100px" }}>
+                <InfoContent>{myRestaurantsCount}개</InfoContent>
+              </div>
+            </InfoBlock>
+            <InfoBlock backgroundColor={"#FFD703"}>
+              <InfoTitle>등록한 나의 위시 맛집 개수</InfoTitle>
+              <div style={{ height: "100px" }}>
+                <InfoContent>{myWishListCount}개</InfoContent>
+              </div>
+            </InfoBlock>
+          </div>
+          <div style={{ display: "flex", justifyContent: "center" }}>
+            <InfoBlock backgroundColor={"#60F5D0"}>
+              <InfoTitle>지역별 나의 방문 맛집</InfoTitle>
+              <div style={{ height: "100px", display: "flex", flexDirection: "column" }}>
+                {
+                  Object.keys(myRestaurantsCntObj).map(area =>
+                    <InfoContent key={area}>{area} {myRestaurantsCntObj[area]}개</InfoContent>
+                  )
+                }
+              </div>
+            </InfoBlock>
+            <InfoBlock backgroundColor={"#FF91A4"}>
+              <InfoTitle>지역별 나의 위시 맛집</InfoTitle>
+              <div style={{ height: "100px", display: "flex", flexDirection: "column" }}>
+                {
+                  Object.keys(myWishlistsCntObj).map(area =>
+                    <InfoContent key={area}>{area} {myWishlistsCntObj[area]}개</InfoContent>
+                  )
+                }
+              </div>
+            </InfoBlock>
+          </div>
+          <div style={{ width: "50%", margin: "auto" }}>
+            <h3>나의 방문 맛집 달력</h3>
+            <div style={{ display: "flex" }}>
+              {
+                colorBar.map((el, index) => <ColorBar key={`colorBar` + index} backgroundColor={el.color}>{el.text}</ColorBar>)
+              }
             </div>
-            <div style={{display: "flex", justifyContent: "center"}}>
-                <InfoBlock backgroundColor={"#5B6DCD"}>
-                    <InfoTitle>등록한 나의 방문 맛집 개수</InfoTitle>
-                    <div style={{height: "100px"}}>
-                        <InfoContent>{myRestaurantsCount}개</InfoContent>
-                    </div>
-                </InfoBlock>
-                <InfoBlock backgroundColor={"#FFD703"}>
-                    <InfoTitle>등록한 나의 위시 맛집 개수</InfoTitle>
-                    <div style={{height: "100px"}}>
-                      <InfoContent>{myWishListCount}개</InfoContent>
-                    </div>
-                </InfoBlock>
-            </div>
-            <div style={{display: "flex", justifyContent: "center"}}>
-                <InfoBlock backgroundColor={"#60F5D0"}>
-                    <InfoTitle>지역별 나의 방문 맛집</InfoTitle>
-                    <div style={{height: "100px", display: "flex", flexDirection: "column"}}>
-                    {
-                      Object.keys(myRestaurantsCntObj).map(area => 
-                        <InfoContent key={area}>{area} {myRestaurantsCntObj[area]}개</InfoContent>
-                      )
-                    }
-                    </div>
-                </InfoBlock>
-                <InfoBlock backgroundColor={"#FF91A4"}>
-                    <InfoTitle>지역별 나의 위시 맛집</InfoTitle>
-                    <div style={{height: "100px", display: "flex", flexDirection: "column"}}>
-                    {
-                      Object.keys(myWishlistsCntObj).map(area => 
-                        <InfoContent key={area}>{area} {myWishlistsCntObj[area]}개</InfoContent>
-                      )
-                    }
-                    </div>
-                </InfoBlock>
-            </div>
-            <div style={{ width: "50%", margin: "auto"}}>
-                <h3>나의 방문 맛집 달력</h3>
-                <div style={{ display: "flex"}}>
-                  {
-                    colorBar.map((el, index) => <ColorBar key={`colorBar` + index} backgroundColor={el.color}>{el.text}</ColorBar>)
-                  }
-                </div>
-                <MyCalendar restaurants={myRestaurants} />
-            </div>
+            <MyCalendar restaurants={myRestaurants} />
+          </div>
         </div>
         <hr />
         <Footer />
