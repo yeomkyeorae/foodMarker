a64b6a1ac8d8faec179ad49645209afc6de56cf2
yeomkyeorae <duarufp06@naver.com>
2022-08-02 21:47:39 +0900

    feat: add color of selected choizaroad menu

--- client-app/src/components/views/ChoizaPage/ChoizaPage.tsx
+++ client-app/src/components/views/ChoizaPage/ChoizaPage.tsx
@@ -10,6 +10,7 @@ import { NavMenuType } from '../../../library/def';
 const Div = styled.div`
   display: inline-block;
   width: 10rem;
+  color: ${props => props.color === 'true' ? '#ff6700' : 'black'};
   &:hover {
     text-shadow: -1px 0 white, 0 1px white, 1px 0 white, 0 -1px white;
     color: #ff6700;
@@ -24,8 +25,8 @@ interface Props {
 
 
 function ChoizaPage({ history }: Props): React.ReactElement {
-  const [season, setSeason] = useState(7);
-  const [seasonName, setSeasonName] = useState("시즌6");
+  const [season, setSeason] = useState(8);
+  const [seasonName, setSeasonName] = useState("시즌7");
 
   const onClickHandler = (seansonName: string, season: number) => {
     setSeason(season);
@@ -45,14 +46,14 @@ function ChoizaPage({ history }: Props): React.ReactElement {
       >
         <NavbarComp history={history} menu={NavMenuType.Choizaroad} />
         <hr />
-        <Div onClick={() => onClickHandler("시즌1", 1)}>시즌1</Div>
-        <Div onClick={() => onClickHandler("시즌2", 2)}>시즌2</Div>
-        <Div onClick={() => onClickHandler("시즌3", 3)}>시즌3</Div>
-        <Div onClick={() => onClickHandler("시즌4", 4)}>시즌4</Div>
-        <Div onClick={() => onClickHandler("인더하우스", 5)}>인더하우스</Div>
-        <Div onClick={() => onClickHandler("시즌5", 6)}>시즌5</Div>
-        <Div onClick={() => onClickHandler("시즌6", 7)}>시즌6</Div>
-        <Div onClick={() => onClickHandler("시즌7", 8)}>시즌7</Div>
+        <Div color={`${season === 1}`} onClick={() => onClickHandler("시즌1", 1)}>시즌1</Div>
+        <Div color={`${season === 2}`} onClick={() => onClickHandler("시즌2", 2)}>시즌2</Div>
+        <Div color={`${season === 3}`} onClick={() => onClickHandler("시즌3", 3)}>시즌3</Div>
+        <Div color={`${season === 4}`} onClick={() => onClickHandler("시즌4", 4)}>시즌4</Div>
+        <Div color={`${season === 5}`} onClick={() => onClickHandler("인더하우스", 5)}>인더하우스</Div>
+        <Div color={`${season === 6}`} onClick={() => onClickHandler("시즌5", 6)}>시즌5</Div>
+        <Div color={`${season === 7}`} onClick={() => onClickHandler("시즌6", 7)}>시즌6</Div>
+        <Div color={`${season === 8}`} onClick={() => onClickHandler("시즌7", 8)}>시즌7</Div>
         <hr />
         <h2 style={{ marginTop: "50px" }}>
           {seasonName}
