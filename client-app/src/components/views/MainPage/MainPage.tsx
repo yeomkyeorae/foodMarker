b758f6dbed9e1412dc428ab11a398a6429188edc
yeomkyeorae <duarufp06@naver.com>
2022-09-16 17:18:27 +0900

    fix: restore removed big div

--- client-app/src/components/views/MainPage/MainPage.tsx
+++ client-app/src/components/views/MainPage/MainPage.tsx
@@ -17,17 +17,6 @@ import { NavMenuType, LocationCodeInfo, LocationCode } from '../../../library/de
 import { BiUser, BiMapPin, BiMale } from "react-icons/bi";
 
 
-const MainDiv = styled.div`
-  width: 100%;
-  height: 100%;
-  text-align: center;
-  position: absolute;
-  top: 10px;
-  left: 0px;
-  right: 0px;
-  overflow: hidden;
-`;
-
 const H2 = styled.h2`
   font-size: 2rem;
 `;
@@ -127,142 +116,152 @@ function MainPage({ history }: Props): React.ReactElement {
   const [mapLevel, setMapLevel] = useState(8);
 
   return (
-    <MainDiv>
-      <NavbarComp history={history} menu={NavMenuType.Main} />
-      <div style={{ backgroundColor: "#FAF7F2" }}>
-        <FoodMarkerTitle>
-          <div>
-            <span style={{ fontWeight: "300", fontSize: "2rem", textAlign: "center" }}>
-              푸드마커로 맛집의 지도를 완성해 보세요
-            </span>
-          </div>
-        </FoodMarkerTitle>
-        <FoodMarkerExplanation>
-          <div style={{ margin: "auto", textAlign: "left" }}>
-            <Link to={{ pathname: "/marker", state: { userId, menu: 2 } }} style={{ textDecoration: "none" }}>
-              <FoodMarkerExplanationTitle>
-                <BiUser />나의 맛집
-              </FoodMarkerExplanationTitle>
-            </Link>
-            <br />
-            <span>당신이 최근에 방문한 최고의 맛집은 어디인가요?</span>
-            <br />
-            <span>인상 깊은 식당을 등록해 보세요.</span>
-          </div>
-          <div style={{ margin: "auto", textAlign: "left" }}>
-            <Link to={{ pathname: "/wish", state: { userId, menu: 3 } }} style={{ textDecoration: "none" }}>
-              <FoodMarkerExplanationTitle>
-                <BiMapPin />위시 맛집
-              </FoodMarkerExplanationTitle>
-            </Link>
-            <br />
-            <span>시선을 사로잡은 맛집은 어디인가요?</span>
-            <br />
-            <span>방문하고자 하는 식당을 기록해 보세요.</span>
-          </div>
-          <div style={{ margin: "auto", textAlign: "left" }}>
-            <Link to={{ pathname: "/choizaroad", state: { userId, menu: 4 } }} style={{ textDecoration: "none" }}>
-              <FoodMarkerExplanationTitle>
-                <BiMale />최자로드
-              </FoodMarkerExplanationTitle>
-            </Link>
-            <br />
-            <span>최자로드에 등장한 맛집이 궁금하신가요?</span>
-            <br />
-            <span>자형과 함께 해보세요.</span>
-            <br />
-            <span></span>
-          </div>
-        </FoodMarkerExplanation>
-      </div>
-      <div style={{ padding: "5px" }}>
-        <H2>나의 맛집 지도</H2>
-        <H3>현재 설정 지역: {LocationCodeInfo[myPlace].locationName}</H3>
-      </div>
-      <div>
-        <KakaoMap
-          latitude={latitude}
-          longitude={longitude}
-          mapLevel={mapLevel}
-          restaurants={restaurants}
-          width={"100%"}
-          height={"480px"}
-          inlineBlock={false}
-        />
-      </div>
+    <div style={{ width: "100%", height: "100%", textAlign: "center" }}>
       <div
         style={{
-          marginBottom: "10px",
-          backgroundColor: "#edfcee",
-          margin: "0"
+          position: "absolute",
+          top: "10px",
+          left: "0px",
+          right: "0px",
+          overflow: "auto"
         }}
       >
-        <div style={{ paddingTop: "20px", marginBottom: "20px" }}>
-          <H2>최근에 등록된 나의 맛집들</H2>
+        <NavbarComp history={history} menu={NavMenuType.Main} />
+        <div style={{ backgroundColor: "#FAF7F2" }}>
+          <FoodMarkerTitle>
+            <div>
+              <span style={{ fontWeight: "300", fontSize: "2rem", textAlign: "center" }}>
+                푸드마커로 맛집의 지도를 완성해 보세요
+              </span>
+            </div>
+          </FoodMarkerTitle>
+          <FoodMarkerExplanation>
+            <div style={{ margin: "auto", textAlign: "left" }}>
+              <Link to={{ pathname: "/marker", state: { userId, menu: 2 } }} style={{ textDecoration: "none" }}>
+                <FoodMarkerExplanationTitle>
+                  <BiUser />나의 맛집
+                </FoodMarkerExplanationTitle>
+              </Link>
+              <br />
+              <span>당신이 최근에 방문한 최고의 맛집은 어디인가요?</span>
+              <br />
+              <span>인상 깊은 식당을 등록해 보세요.</span>
+            </div>
+            <div style={{ margin: "auto", textAlign: "left" }}>
+              <Link to={{ pathname: "/wish", state: { userId, menu: 3 } }} style={{ textDecoration: "none" }}>
+                <FoodMarkerExplanationTitle>
+                  <BiMapPin />위시 맛집
+                </FoodMarkerExplanationTitle>
+              </Link>
+              <br />
+              <span>시선을 사로잡은 맛집은 어디인가요?</span>
+              <br />
+              <span>방문하고자 하는 식당을 기록해 보세요.</span>
+            </div>
+            <div style={{ margin: "auto", textAlign: "left" }}>
+              <Link to={{ pathname: "/choizaroad", state: { userId, menu: 4 } }} style={{ textDecoration: "none" }}>
+                <FoodMarkerExplanationTitle>
+                  <BiMale />최자로드
+                </FoodMarkerExplanationTitle>
+              </Link>
+              <br />
+              <span>최자로드에 등장한 맛집이 궁금하신가요?</span>
+              <br />
+              <span>자형과 함께 해보세요.</span>
+              <br />
+              <span></span>
+            </div>
+          </FoodMarkerExplanation>
         </div>
-        <OrderList>
-          {tenRestaurants.map((el, ix) => (
-            <List key={ix}>
-              <div>
-                <img
-                  src={el.imgURL.split(",")[0]}
-                  alt=""
-                  width="180px"
-                  height="200px"
-                />
-              </div>
-              <div>
+        <div style={{ padding: "5px" }}>
+          <H2>나의 맛집 지도</H2>
+          <H3>현재 설정 지역: {LocationCodeInfo[myPlace].locationName}</H3>
+        </div>
+        <div>
+          <KakaoMap
+            latitude={latitude}
+            longitude={longitude}
+            mapLevel={mapLevel}
+            restaurants={restaurants}
+            width={"100%"}
+            height={"480px"}
+            inlineBlock={false}
+          />
+        </div>
+        <div
+          style={{
+            marginBottom: "10px",
+            backgroundColor: "#edfcee",
+            margin: "0"
+          }}
+        >
+          <div style={{ paddingTop: "20px", marginBottom: "20px" }}>
+            <H2>최근에 등록된 나의 맛집들</H2>
+          </div>
+          <OrderList>
+            {tenRestaurants.map((el, ix) => (
+              <List key={ix}>
                 <div>
-                  <p
-                    style={{
-                      fontWeight: "500",
-                      fontSize: "1.5rem",
-                      marginBottom: "5px"
-                    }}
-                  >
-                    {el.name}
+                  <img
+                    src={el.imgURL.split(",")[0]}
+                    alt=""
+                    width="180px"
+                    height="200px"
+                  />
+                </div>
+                <div>
+                  <div>
+                    <p
+                      style={{
+                        fontWeight: "500",
+                        fontSize: "1.5rem",
+                        marginBottom: "5px"
+                      }}
+                    >
+                      {el.name}
+                    </p>
+                    <p style={{ fontWeight: "300", margin: "0" }}>
+                      {el.address}
+                    </p>
+                    <p style={{ fontWeight: "400", margin: "0" }}>
+                      {el.username}
+                    </p>
+                  </div>
+                </div>
+              </List>
+            ))}
+          </OrderList>
+        </div>
+        <div
+          style={{
+            marginBottom: "10px",
+            backgroundColor: "#FAFDF3",
+          }}
+        >
+          <div style={{ paddingTop: "20px", marginBottom: "20px" }}>
+            <H2>최근에 등록된 위시 맛집들</H2>
+          </div>
+          <OrderList>
+            {tenWishList.map((wishListItem, ix) => (
+              <List key={ix}>
+                <div style={{ marginBottom: "5px" }}>
+                  <p style={{ fontWeight: "500", fontSize: "1.5rem", margin: "0", whiteSpace: "nowrap" }}>
+                    {wishListItem.name}
                   </p>
                   <p style={{ fontWeight: "300", margin: "0" }}>
-                    {el.address}
+                    {wishListItem.address}
                   </p>
                   <p style={{ fontWeight: "400", margin: "0" }}>
-                    {el.username}
+                    {wishListItem.username}
                   </p>
                 </div>
-              </div>
-            </List>
-          ))}
-        </OrderList>
-      </div>
-      <div
-        style={{
-          marginBottom: "10px",
-          backgroundColor: "#FAFDF3",
-        }}
-      >
-        <div style={{ paddingTop: "20px", marginBottom: "20px" }}>
-          <H2>최근에 등록된 위시 맛집들</H2>
+              </List>
+            ))}
+          </OrderList>
         </div>
-        <OrderList>
-          {tenWishList.map((wishListItem, ix) => (
-            <List key={ix}>
-              <div style={{ marginBottom: "5px" }}>
-                <p style={{ fontWeight: "500", fontSize: "1.5rem", margin: "0", whiteSpace: "nowrap" }}>
-                  {wishListItem.name}
-                </p>
-                <p style={{ fontWeight: "300", margin: "0" }}>
-                  {wishListItem.address}
-                </p>
-                <p style={{ fontWeight: "400", margin: "0" }}>
-                  {wishListItem.username}
-                </p>
-              </div>
-            </List>
-          ))}
-        </OrderList>
+        <Footer />
       </div>
-      <Footer />
-    </MainDiv>
+    </div>
   );
 }
 
--- client-app/src/components/views/MarkerPage/MarkerPage.tsx
+++ client-app/src/components/views/MarkerPage/MarkerPage.tsx
@@ -6,19 +6,6 @@ import NavbarComp from "../Navbar/NavbarComp";
 import Footer from "../Footer/Footer";
 import { Button } from "react-bootstrap";
 import { NavMenuType } from '../../../library/def';
-import styled from "styled-components";
-
-
-const MainDiv = styled.div`
-  width: 100%;
-  height: 100%;
-  text-align: center;
-  position: absolute;
-  top: 10px;
-  left: 0px;
-  right: 0px;
-  overflow: hidden;
-`;
 
 interface Props {
   history: RouteComponentProps["history"];
@@ -51,22 +38,32 @@ function MarkerPage({ history }: Props): React.ReactElement {
   }
 
   return (
-    <MainDiv>
-      <NavbarComp history={history} menu={NavMenuType.Marker} />
-      <hr />
-      <div>
-        <Button
-          variant="primary"
-          onClick={onClickChangeMenuHandler}
-          style={{ margin: "20px" }}
-        >
-          {Menu}
-        </Button>
+    <div style={{ width: "100%", height: "100%", textAlign: "center" }}>
+      <div
+        style={{
+          position: "absolute",
+          top: "10px",
+          left: "0px",
+          right: "0px",
+          overflow: "auto"
+        }}
+      >
+        <NavbarComp history={history} menu={NavMenuType.Marker} />
+        <hr />
+        <div>
+          <Button
+            variant="primary"
+            onClick={onClickChangeMenuHandler}
+            style={{ margin: "20px" }}
+          >
+            {Menu}
+          </Button>
+        </div>
+        <hr />
+        {MenuComponent}
+        <Footer />
       </div>
-      <hr />
-      {MenuComponent}
-      <Footer />
-    </MainDiv>
+    </div>
   );
 }
 
