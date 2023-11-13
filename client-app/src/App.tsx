d1fdc22e76955374139b93fb5627918bba690310
Kyeorae Yeom <duarufp06@gmail.com>
2023-11-13 23:15:52 +0900

    tmp

--- client-app/src/App.tsx
+++ client-app/src/App.tsx
@@ -2,14 +2,14 @@ import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
+import Wish from './pages/wishlist/Wish';
+import MyInfo from './pages/myInfo/MyInfo';
 import LandingPage from './pages/landing/LadingPage';
 import LoginSignup from './pages/sign/LoginSignup';
 import MainPage from './pages/main/MainPage';
 import CurrentLocation from './pages/nearby/CurrentLocation';
 import MarkerPage from './pages/marker/MarkerPage';
-import Wish from './pages/wishlist/Wish';
 import ChoizaPage from './pages/choizeRoad/ChoizaPage';
-import MyInfo from './pages/myInfo/MyInfo';
 import Auth from './context/auth';
 
 function App() {
@@ -17,12 +17,12 @@ function App() {
     <Router>
       <Switch>
         <Route exact path='/' component={Auth(<LandingPage />, null)} />
+        <Route exact path='/wish' component={Auth(<Wish />, true)} />
+        <Route exact path='/my-info' component={Auth(<MyInfo />, true)} />
         <Route exact path='/loginSignup' component={Auth(<LoginSignup />, false)} />
         <Route exact path='/main' component={Auth(<MainPage />, true)} />
         <Route exact path='/current-location' component={Auth(<CurrentLocation />, true)} />
         <Route exact path='/marker' component={Auth(<MarkerPage />, true)} />
-        <Route exact path='/wish' component={Auth(<Wish />, true)} />
-        <Route exact path='/my-info' component={Auth(<MyInfo />, true)} />
         <Route exact path='/choizaroad' component={Auth(<ChoizaPage />, true)} />
       </Switch>
     </Router>
